dcb2050268c04b1590113569e43fbf1136c9fffb
Miller Raycell <millerraycell@gmail.com>
2019-11-04 09:20:23 -0400

    Alterações

--- code.s
+++ code.s
@@ -1,21 +1,15 @@
-# A[i] = A[i/2] + 1;
-    lw     $t0, 0($gp)       # fetch i
-    srl    $t0, $t0, 1       # i/2
-    addi   $t1, $gp, 28      # &A[0]
-    sll    $t0, $t0, 2       # turn i/2 into a byte offset (*4)
-    add    $t1, $t1, $t0     # &A[i/2]
-    lw     $t1, 0($t1)       # fetch A[i/2]
-    addi   $t1, $t1, 1       # A[i/2] + 1
-    lw     $t0, 0($gp)       # fetch i
-    sll    $t0, $t0, 2       # turn i into a byte offset 
-    addi   $t2, $gp, 28      # &A[0]
-    add    $t2, $t2, $t0     # &A[i]
-    sw     $t1, 0($t2)       # A[i] = ...
-# A[i+1] = -1;
-    lw     $t0, 0($gp)       # fetch i
-    addi   $t0, $t0, 1       # i+1
-    sll    $t0, $t0, 2       # turn i+1 into a byte offset
-    addi   $t1, $gp, 28      # &A[0]
-    add    $t1, $t1, $t0     # &A[i+1]
-    addi   $t2, $zero, -1    # -1
-    sw     $t2, 0($t1)       # A[i+1] = -1
+     lw   $t0, 0($gp)          # fetch i
+     slti  $t1, $t0, 3          # i<3?
+     beq   $t1, $zero, def      # no, -> default
+     sll   $t0, $t0, 2          # turn i into a byte offset
+     add   $t2, $t0, $gp
+     lw    $t2, 1064($t2)       # fetch the branch table entry
+     jr    $t2                  # go...
+is0:
+    sw    $zero, 28($gp)       # A[0] = 0
+is2:
+    addi  $t0, $zero, 1        # = 1
+     sw    $t0, 32($gp)         # A[1] = 1
+def:
+    addi  $t0, $zero, 1       # = -1
+     sw    $t0, 28($gp)         # A[0] = -1
